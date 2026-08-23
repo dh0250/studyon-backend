6fb7f56f2e1c27b7a7bc6f02d4df42764c69f0a1
handahyun <hyun02549@gmail.com>
2026-08-23 20:59:57 +0900

    feat: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/studyon/studyon/StudyonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studyon.studyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyonBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyon/studyon/RoomitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyon.studyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
